4f308aa6e7a7e4b5644ce698fd64db79c994ce4d
Aryadip Mridha <75301024+AryadipMridha@users.noreply.github.com>
2022-05-07 01:16:26 +0530

    initial

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Hello there! Welcome to Band Name Genrator software\n')
+city=input('Enter the city you grew up?\n')
+pet=input('Enter the first pet you had? \n')
+print('Your band name could be '+city+' '+pet)
